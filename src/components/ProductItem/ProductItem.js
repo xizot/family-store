aa67d9af7212fd5f5beae9f4e4eb26a33f8016e6
Hieu dang <dangtrunghieu2304@gmail.com>
2021-07-15 14:32:32 +0700

    fix prodcut item

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -1,16 +1,19 @@
 import {
-    CardContent, IconButton, makeStyles, Typography, Card
-    , CardMedia, CardActions, Box
+    CardContent, IconButton, makeStyles, Typography, Card,
+    CardHeader, CardMedia, Box
 } from "@material-ui/core";
 import React from "react";
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import ButtonNumeric from "../UI/NumericUpDown";
+import { Link } from "react-router-dom";
 import MoreVertIcon from '@material-ui/icons/Info';
 
 
 const useStyles = makeStyles((theme) => ({
     root: {
         maxWidth: 220,
-        height: 250,
+        height: 240,
+        position:"relative"
     },
     media: {
         height: 0,
@@ -24,38 +27,42 @@ const useStyles = makeStyles((theme) => ({
         marginLeft: 'auto',
     },
     action: {
-        padding:'0 8px 4px 8px'
+        position:"absolute",
+        zIndex:1,
+        padding: '0 8px 0 0',
+        right:0
     }
 }));
 
-const ProductItem = ({ title, price, description, quantity, url, onAdd, onRemove, onDetail }) => {
+const ProductItem = (props) => {
     const classes = useStyles();
 
     return (
         <Box>
             <Card className={classes.root}>
-                <CardMedia
-                    className={classes.media}
-                    image="https://cdn.tgdd.vn/2020/07/content/bo-anh-yasuo-lol-dep-va-chat-de-lam-hinh-nen-dien-thoai-may1-800x450.jpg"
-                    title="Paella dish"
+                <CardHeader
+                    className={classes.action}
+                    action={
+                        <IconButton aria-label="settings">
+                            <ShoppingCartIcon />
+                        </IconButton>
+                    }
                 />
-                <CardContent>
-                    <Typography>$0.45</Typography>
-                    <Typography variant="body2" color="textSecondary" component="p" className={classes.description}>
-                        Cần sa 10 tấn
-                    </Typography>
-                </CardContent>
-                <CardActions disableSpacing className={classes.action}>
-                    <ButtonNumeric
-                        quantity={10}
+                <Link to="/details/id" >
+                    <CardMedia
+                        className={classes.media}
+                        image="https://cdn.tgdd.vn/2020/07/content/bo-anh-yasuo-lol-dep-va-chat-de-lam-hinh-nen-dien-thoai-may1-800x450.jpg"
+                        title="Paella dish"
                     />
-                    <IconButton
-                        className={classes.expand}
-                    >
-                        <MoreVertIcon />
-                    </IconButton>
-                </CardActions >
+                     </Link>
+                    <CardContent>
+                        <Typography>$0.45</Typography>
+                        <Typography variant="body2" color="textSecondary" component="p" className={classes.description}>
+                            Cần sa 10 tấn
+                        </Typography>
+                    </CardContent>         
             </Card>
+
         </Box>
 
     )
