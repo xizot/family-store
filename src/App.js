9d676b8a4d02d5324c3ec734a2ef82990a70e36d
ngocsotn <nguyenthengoc1998@live.com>
2021-07-28 16:57:18 +0700

    rename some routes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
-import { createTheme, ThemeProvider } from "@material-ui/core";
-import { lazy, Suspense, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Route, Switch } from "react-router-dom";
-import Cart from "./components/Cart/Cart";
-import { ProtectedRoute } from "./components/Common/ProtectedRoute";
-import Loading from "./components/Loading/Loading";
-import { useTranslation } from "react-i18next";
-import { langActions } from "./reducers/lang";
-const ProductDetail = lazy(() => import("./pages/ProductDetail"));
-const HomePage = lazy(() => import("./pages/HomePage"));
-const LoginPage = lazy(() => import("./pages/LoginPage"));
-const RegisterPage = lazy(() => import("./pages/RegisterPage"));
-const ForgotPasswordPage = lazy(() => import("./pages/forgotPassword"));
-const RecoveryPasswordPage = lazy(() => import("./pages/recoveryPassword"));
-const AccountActivationPage = lazy(() => import("./pages/accountActivation"));
-const ProfilePage = lazy(() => import("./pages/Profile"));
-const PageNotFound = lazy(() => import("./pages/404NotFound"));
-const SearchPage = lazy(() => import("./pages/SearchPage"));
-const CollectionsPage = lazy(() => import("./pages/Collections"));
+import { createTheme, ThemeProvider } from '@material-ui/core';
+import { lazy, Suspense, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import Cart from './components/Cart/Cart';
+import { ProtectedRoute } from './components/Common/ProtectedRoute';
+import Loading from './components/Loading/Loading';
+import { useTranslation } from 'react-i18next';
+import { langActions } from './reducers/lang';
+const ProductDetail = lazy(() => import('./pages/ProductDetail'));
+const HomePage = lazy(() => import('./pages/HomePage'));
+const LoginPage = lazy(() => import('./pages/LoginPage'));
+const RegisterPage = lazy(() => import('./pages/RegisterPage'));
+const ForgotPasswordPage = lazy(() => import('./pages/ForgotPassword'));
+const RecoveryPasswordPage = lazy(() => import('./pages/RecoveryPassword'));
+const AccountActivationPage = lazy(() => import('./pages/AccountActivation'));
+const ProfilePage = lazy(() => import('./pages/Profile'));
+const PageNotFound = lazy(() => import('./pages/404NotFound'));
+const SearchPage = lazy(() => import('./pages/SearchPage'));
+const CollectionsPage = lazy(() => import('./pages/Collections'));
 
 const theme = createTheme({
 	palette: {
 		primary: {
-			main: "#F39148",
-			contrastText: "#fff",
-		},
-	},
+			main: '#F39148',
+			contrastText: '#fff'
+		}
+	}
 });
 function App() {
 	const dispatch = useDispatch();
 	const { i18n } = useTranslation();
 	const isOpenCart = useSelector((state) => state.ui.isOpenCart);
-	useEffect(() => {
-		const existingLang = localStorage.getItem("lang");
-		if (!existingLang || (existingLang !== "vn" && existingLang !== "en"))
-			return;
+	useEffect(
+		() => {
+			const existingLang = localStorage.getItem('lang');
+			if (!existingLang || (existingLang !== 'vn' && existingLang !== 'en')) return;
 
-		dispatch(langActions.updateLang(existingLang));
+			dispatch(langActions.updateLang(existingLang));
 
-		i18n.changeLanguage(existingLang);
-	}, [i18n, dispatch]);
+			i18n.changeLanguage(existingLang);
+		},
+		[ i18n, dispatch ]
+	);
 	return (
 		<ThemeProvider theme={theme}>
 			{isOpenCart && <Cart />}
@@ -48,16 +50,8 @@ function App() {
 					<Route exact path="/">
 						<HomePage />
 					</Route>
-					<ProtectedRoute
-						exact
-						path="/profile"
-						component={ProfilePage}
-					/>
-					<ProtectedRoute
-						exact
-						path="/profile/:slug"
-						component={ProfilePage}
-					/>
+					<ProtectedRoute exact path="/profile" component={ProfilePage} />
+					<ProtectedRoute exact path="/profile/:slug" component={ProfilePage} />
 					<Route exact path="/login">
 						<LoginPage />
 					</Route>
