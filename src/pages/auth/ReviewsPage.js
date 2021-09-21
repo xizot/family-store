09151ba89b19f3e1791495634fa7b814e042c901
ngocsotn <nguyenthengoc1998@live.com>
2021-09-21 13:53:38 +0700

    fix: bug language

--- src/pages/auth/ReviewsPage.js
+++ src/pages/auth/ReviewsPage.js
@@ -204,9 +204,9 @@ const ReviewsPage = (props) => {
                 <Grid item xs={12} sm={12} md={1}>
                   <div className={classes.TopContentDetailsLeft}>
                     <div className={classes.ChildPropertiesLabel}>
-                      <Typography variant="body1"> {t('checkoutPage.address.namePlaceHolder')?.toUpperCase()}: </Typography>
+                      <Typography variant="body1"> {t('checkoutPage.address.form.namePlaceHolder')?.toUpperCase()}: </Typography>
                       <Typography variant="body1">  {t('profilepage.address')?.toUpperCase()}: </Typography>
-                      <Typography variant="body1"> {t('checkoutPage.address.phoneNumberPlaceHolder')?.toUpperCase()}: </Typography>
+                      <Typography variant="body1"> {t('checkoutPage.address.form.phoneNumberPlaceHolder')?.toUpperCase()}: </Typography>
                     </div>
                   </div>
                 </Grid>
